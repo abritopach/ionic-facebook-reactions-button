16092bda5c5be0be364696b766a90cc243a0c291
Adrián Brito <adrbrpa1988@gmail.com>
2017-09-18 17:10:52 +0200

    Updated

--- src/pages/reactions/reactions.ts
+++ src/pages/reactions/reactions.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 /**
  * Generated class for the ReactionsPage page.
@@ -12,18 +13,45 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 @Component({
     selector: 'page-reactions',
     templateUrl: 'reactions.html',
+    animations: [
+        trigger('scaleReactionIcon', [
+            state('idle', style({
+                transform: 'scale(1)'
+            })),
+            state('clicking', style({
+                transform: 'scale(1.5)'
+            })),
+            transition('idle <=> clicking', animate('100ms linear')),
+        ]),
+    ]
 })
 export class ReactionsPage {
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController) {
+    reactionIcons: Object[] = []
+
+    constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController,
+                private changeDetector: ChangeDetectorRef) {
+        this.reactionIcons = [
+            {iconImage: 'assets/img/like.png', iconState: 'idle'},
+            {iconImage: 'assets/img/love.png', iconState: 'idle'},
+            {iconImage: 'assets/img/haha.png', iconState: 'idle'},
+            {iconImage: 'assets/img/wow.png', iconState: 'idle'},
+            {iconImage: 'assets/img/sad.png', iconState: 'idle'},
+            {iconImage: 'assets/img/angry.png', iconState: 'idle'}
+        ];
     }
 
     ionViewDidLoad() {
         console.log('ionViewDidLoad ReactionsPage');
     }
 
-    share(){
-        this.viewCtrl.dismiss();
+    share(icon) {
+        icon.iconState = 'clicking';
+        this.changeDetector.detectChanges();
+
+        setTimeout(() => {
+            this.viewCtrl.dismiss();
+        }, 300);
     }
 
 }
